839eadc06771ba30f3dd6418c3e2f5315278ea38
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-10-24 22:10:08 +0200

    attempt to solve herokue proxy issue

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,9 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
      
 module.exports = function(app) {
 
-    const backendHost = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
-
-    app.use('/api-issuer/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
-    app.use('/api-credential-service/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
-    app.use('/api-ipfs/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+    if(process.env.NODE_ENV !== 'production'){
+        const backendHost = 'http://localhost:3001';
+        app.use('/api-issuer/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+        app.use('/api-credential-service/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+        app.use('/api-ipfs/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+    }
 };
