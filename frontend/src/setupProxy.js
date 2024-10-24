1d942cce2038c391c780dc9186a43e5d19d15776
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-10-24 22:00:42 +0200

    need to adapt manual proxy for heroku

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,7 +1,10 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
      
 module.exports = function(app) {
-    app.use('/api-issuer/*', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true}));
-    app.use('/api-credential-service/*', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true}));
-    app.use('/api-ipfs/*', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true}));
+
+    const backendHost = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3001';
+
+    app.use('/api-issuer/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+    app.use('/api-credential-service/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
+    app.use('/api-ipfs/*', createProxyMiddleware({ target: backendHost, changeOrigin: true}));
 };
