cb925eddf47ffd5d283cd35d8c2ca03d77dddca0
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-10-28 12:23:54 +0100

    beautified access mananger page

--- frontend/src/components/AccessManager.js
+++ frontend/src/components/AccessManager.js
@@ -4,13 +4,11 @@ const AccessManager = () => {
         <h1>Private IPFS Access Manager</h1>
         <h2>- Mange Your Private Supply Chain Event Metadata files stored in Private IPFS powered by Pinata. -</h2>
         <div>
-            Functionalities:
-            <li>
-                <ul>Issue Private IPFS Access Credentials to others</ul>
-                <ul>Revoke your issued Private IPFS Access Credentials</ul>
-                <ul>View your private IPFS Files and their authorized accessors</ul>
-                <ul>View your issued Private IPFS Access Credentials and their revocation status</ul>
-            </li>
+            Functionalities of the Private IPFS Access Manager:
+            <li>Issue Private IPFS Access Credentials to others</li>
+            <li>Revoke your issued Private IPFS Access Credentials</li>
+            <li>View your private IPFS Files and their authorized accessors</li>
+            <li>View your issued Private IPFS Access Credentials and their revocation status</li>
         </div>
       </div>
     );
