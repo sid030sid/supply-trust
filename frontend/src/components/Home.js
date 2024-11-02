c66db81963979c08dc486250a582f54ce0d12e7f
sid030sid <78762408+sid030sid@users.noreply.github.com>
2024-11-02 15:09:28 +0100

    finished final read me

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,7 +2,7 @@ const Home = () => {
     return(
       <div>
         <h1>SupplyTrust</h1>
-        <h5>- A Verifiable Supply Chain Data Management System For Tracing, Tracking, And Trusting Supply Chains -</h5>
+        <h5>- A Verifiable Supply Chain Data Management Tool For Tracing, Tracking, And Trusting Supply Chains -</h5>
       </div>
     );
   };
